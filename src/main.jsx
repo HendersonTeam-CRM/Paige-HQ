a32ad74185aa5d60fc2d3901b4105811908cda03
HendersonTeam-CRM <henderson322@icloud.com>
2026-08-01 20:31:58 -0400

    Rename App.jsx to app.jsx in import statement

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import PaigeHQ from "./App.jsx";
+import PaigeHQ from "./app.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
